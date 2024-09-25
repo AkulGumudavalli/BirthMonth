e98777fa09093d93be16d6f76db3747c5206572e
AkulGumudavall <gumudaau@mail.uc.edu>
2024-09-24 20:24:03 -0400

    Finished Task 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is your birth month");
+        double birthMonth = input.nextInt();
+        if(birthMonth > 12){
+            System.out.println("You entered an incorrect month value: "+ birthMonth);
+        }
+        else{
+            System.out.println("Your birth month is: "+ birthMonth);
+        }
+
 
     }
 }
