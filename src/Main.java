04c550c6a51535363dac571e681e68186f686f0b
AkulGumudavall <gumudaau@mail.uc.edu>
2024-09-29 15:51:54 -0400

    Fixed stuff

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,25 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        // num birthMonth
+        int birthMonth;
+        //Output "What is your birth month"
         System.out.println("What is your birth month");
-        double birthMonth = input.nextInt();
-        if(birthMonth > 12){
-            System.out.println("You entered an incorrect month value: "+ birthMonth);
-        }
-        else{
-            System.out.println("Your birth month is: "+ birthMonth);
-        }
+        if(input.hasNextInt()) {
+            //Input birthMonth
+            birthMonth = input.nextInt();
+            //if birthMonth > 12 then
+                //Output "You entered an incorrect month value: " + birthMonth
+            if (birthMonth > 12) {
+                System.out.println("You entered an incorrect month value: " + birthMonth);
+            }
+            //else
+                //Output "Your birth month is: " + birthMonth
+            else {
+                System.out.println("Your birth month is: " + birthMonth);
+            }
+            //endif
 
-
+        }
     }
 }
